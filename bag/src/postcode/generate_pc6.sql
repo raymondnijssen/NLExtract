4b9c400965dbd8e92f09f9d4552c54b3410c2e74
Raymond Nijssen <r.nijssen@terglobo.nl>
2019-04-08 09:14:33 +0200

    postcode6 speedup (3u45)

--- bag/src/postcode/generate_pc6.sql
+++ bag/src/postcode/generate_pc6.sql
@@ -2,117 +2,84 @@
 
 
 
--- unieke adrespunten (25m20)
-RAISE NOTICE 'Generating adrespunten: %', now();
- 
+-- unieke adreslocaties (25m20)
 
-DROP TABLE IF EXISTS postcode.adrespunten;
-CREATE TABLE postcode.adrespunten AS
+DROP TABLE IF EXISTS postcode.adreslocaties;
+CREATE TABLE postcode.adreslocaties (
+	wp_gid integer REFERENCES bagactueel.woonplaats(gid),
+	min_postcode char(6),
+	all_postcodes char(6)[],
+	adres_count integer,
+	geom geometry(Point, 28992)
+);
+
+INSERT INTO postcode.adreslocaties
 SELECT
-  wp.identificatie AS wp_identificatie,
-  min(a.postcode) AS postcode,
-  ARRAY_AGG(DISTINCT a.postcode) AS postcodes,
+  wp.gid AS wp_gid,
+  min(a.postcode) AS min_postcode,
+  ARRAY_AGG(DISTINCT a.postcode) AS all_postcodes,
   count(*) AS punt_count,
-  a.geopunt AS a_geom,
-  wp.geovlak AS wp_geom
+  ST_Force2D(a.geopunt) AS geom
 FROM bagactueel.adres AS a
 LEFT JOIN bagactueel.woonplaatsactueelbestaand AS wp ON ST_Within(a.geopunt, wp.geovlak)
---WHERE wp.identificatie::integer < 1010 -- 3086 --3594 --= 1108
-GROUP BY wp.identificatie, a.geopunt, wp.geovlak;
---ORDER BY count(*) DESC;
---LIMIT 10 
+--WHERE wp.identificatie::integer = 1007 -- 3086 --3594 --= 1108
+GROUP BY wp.gid, a.geopunt;
 
-CREATE INDEX postcode_adrespunten_wp_identificatie ON postcode.adrespunten (wp_identificatie);
-CREATE INDEX postcode_adrespunten_postcode ON postcode.adrespunten (postcode);
-CREATE INDEX postcode_adrespunten_a_geom ON postcode.adrespunten USING GIST (a_geom);
-CREATE INDEX postcode_adrespunten_wp_geom ON postcode.adrespunten USING GIST (wp_geom);
+CREATE INDEX postcode_adrespunten_wp_gid ON postcode.adreslocaties (wp_gid);
+CREATE INDEX postcode_adrespunten_min_postcode ON postcode.adreslocaties (min_postcode);
+CREATE INDEX postcode_adrespunten_geom ON postcode.adreslocaties USING GIST (geom);
 
+--SELECT wp_gid, count(*) FROM postcode.adreslocaties GROUP BY wp_gid ORDER BY wp_gid LIMIT 10;
 
 
+
+
+-- voronoi adreslocaties
 
 DROP TABLE IF EXISTS postcode.pc_voronoi;
-CREATE TABLE postcode.pc_voronoi AS
+
+CREATE TABLE postcode.pc_voronoi (
+	wp_gid integer REFERENCES bagactueel.woonplaats(gid),
+	geom geometry(MultiPolygon, 28992)
+);
+
+INSERT INTO postcode.pc_voronoi
 SELECT
-  wp_identificatie,
-  ST_Buffer((ST_Dump(ST_VoronoiPolygons(ST_Union(a_geom), 0, wp_geom))).geom, 0) AS geom
-FROM postcode.adrespunten
---WHERE wp_identificatie::integer < 1100 --= 3594 = 1108
-GROUP BY wp_identificatie, wp_geom;
+  wp.gid as wp_gid,
+  ST_Multi(ST_Buffer((ST_Dump(ST_VoronoiPolygons(ST_Union(a.geom), 0, wp.geovlak))).geom, 0)) AS geom
+FROM postcode.adreslocaties AS a
+INNER JOIN bagactueel.woonplaats AS wp ON (wp.gid = a.wp_gid)
+GROUP BY wp.gid;
 
-SELECT wp_identificatie, count(*)
-FROM postcode.pc_voronoi
-GROUP BY wp_identificatie
-ORDER BY wp_identificatie;
-
-CREATE INDEX postcode_pc_voronoi_wp_identificatie ON postcode.pc_voronoi (wp_identificatie);
+CREATE INDEX postcode_pc_voronoi_wp_gid ON postcode.pc_voronoi (wp_gid);
 CREATE INDEX postcode_pc_voronoi_geom ON postcode.pc_voronoi USING GIST (geom);
 
 
 
-DROP TABLE IF EXISTS postcode.pc6_union;
-CREATE TABLE postcode.pc6_union AS
 
+-- postcode 6
+
+DROP TABLE IF EXISTS postcode.postcode6;
+CREATE TABLE postcode.postcode6 AS
 SELECT
-  a.wp_identificatie,
-  a.postcode,
-  sum(a.punt_count) AS punt_count,
-  count(*) AS adres_count,
-  ST_Intersection(ST_Union(pcv.geom), a.wp_geom) AS geom
-FROM postcode.pc_voronoi pcv
-LEFT JOIN postcode.adrespunten AS a ON (
-  a.wp_identificatie = pcv.wp_identificatie
+  wp.gid AS wp_gid,
+  a.min_postcode,
+  --a.all_postcodes,
+  sum(a.adres_count) AS adres_count,
+  ST_Intersection(ST_Union(vor.geom), wp.geovlak) AS geom
+FROM postcode.pc_voronoi AS vor
+INNER JOIN postcode.adreslocaties AS a ON (
+  a.wp_gid = vor.wp_gid
     AND 
-  ST_Within(a.a_geom, pcv.geom))
-GROUP BY a.wp_identificatie, a.postcode, a.wp_geom;
+  ST_Within(a.geom, vor.geom))
+INNER JOIN bagactueel.woonplaats AS wp ON (wp.gid = a.wp_gid)  
+GROUP BY wp.gid, a.min_postcode;
 
-
---SELECT * FROM postcode.pc6_union WHERE not punt_count = adres_count
-
-
-
---
-
-DROP TABLE IF EXISTS postcode.pc6_union;
-CREATE TABLE postcode.pc6_union AS
-SELECT 
-  pc6u.wp_identificatie,
-  pc6u.postcode,
-  pc6u.punt_count,
-  pc6u.adres_count,
-  ST_Intersection(pc6u.geom, wp.geovlak) AS geom
-FROM
-(
-SELECT
-  a.wp_identificatie,
-  a.postcode,
-  sum(a.punt_count) AS punt_count,
-  count(*) AS adres_count,
-  ST_Union(pcv.geom) AS geom
-FROM postcode.pc_voronoi pcv
-LEFT JOIN postcode.adrespunten AS a ON (
-  a.wp_identificatie = pcv.wp_identificatie
-    AND 
-  ST_Within(a.a_geom, pcv.geom))
-GROUP BY a.wp_identificatie, a.postcode
-) pc6u
-LEFT JOIN bagactueel.woonplaatsactueelbestaand wp ON wp.identificatie = pc6u.wp_identificatie;
---ORDER BY pc6u.postcode
+CREATE INDEX postcode_postcode6_wp_gid ON postcode.postcode6 (wp_gid);
+CREATE INDEX postcode_postcode6_geom ON postcode.postcode6 USING GIST (geom);
 
 
 
+--DROP TABLE IF EXISTS postcode.adreslocaties;
+--DROP TABLE IF EXISTS postcode.pc_voronoi;
 
-/*
-
-SELECT * FROM bagactueel.nummeraanduiding
-WHERE 
-  begindatumtijdvakgeldigheid <= NOW()
-    AND
-  (
-    einddatumtijdvakgeldigheid IS NOT NULL
-      AND
-    einddatumtijdvakgeldigheid >= NOW()
-   )
-ORDER BY einddatumtijdvakgeldigheid DESC
-LIMIT 10
-
-*/
